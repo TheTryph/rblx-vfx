b3e115f149000b860b57433a43bdfc61018b8630
TheTryph <ihab020907@gmail.com>
2022-07-16 12:00:55 +0100

    fixed the packages repository

--- compiler.lua
+++ compiler.lua
@@ -3,11 +3,12 @@ local HTTPASSETS = {
     ["newPart"] = "https://raw.githubusercontent.com/TheTryph/rblx-vfx/main/newPart.lua?token=GHSAT0AAAAAABWVDN6KLPOEZKKR2UEZY2VQYWSTFUA"
 }
 
+local Packages = Instance.new("Folder", game.ReplicatedStorage)
+Packages.Name = "Packages"
+local Repo = Instance.new("Folder", Packages)
+Repo.Name = "Repo"
+
 for i,v in pairs(HTTPASSETS) do
-    local Packages = Instance.new("Folder", game.ReplicatedStorage)
-    Packages.Name = "Packages"
-    local Repo = Instance.new("Folder", Packages)
-    Repo.Name = "Repo"
     local HTTPSERVICE = game:GetService("HttpService")
     local ScriptContent = HTTPSERVICE:GetAsync(v)
     local ModuleScript = Instance.new("ModuleScript", Packages)
