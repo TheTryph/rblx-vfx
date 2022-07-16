578bd6fa1390835959a8ee365c086d14aa0d6fea
TheTryph <ihab020907@gmail.com>
2022-07-16 12:24:09 +0100

    Added new functions and tween

---
+++ Tween.lua
@@ -0,0 +1,31 @@
+--[[
+
+INSTRUCTIONS:
+
+How to use
+Each function contains the following parameters:
+dur - Duration of the tween
+prop - Table property
+inst - Instance to tween
+
+Functions:
+
+tween(dur, prop, inst) - An ordinary tween with the Enum.EasingStyle.Sine as easingstyle
+
+
+
+
+
+
+
+]]
+
+local tween = {}
+local tweenService = game:GetService("TweenService")
+
+function tween.tween(dur, prop, inst)
+	local tweenInfo = TweenInfo.new(dur, Enum.EasingStyle.Sine,Enum.EasingDirection.Out, 0)
+	tweenService:Create(inst, tweenInfo, prop):Play()
+end
+
+return tween
--- initialLua.lua
+++ initialLua.lua
@@ -1,14 +1,60 @@
 local module = {}
 local modulesFolder = game.ReplicatedStorage.Packages.Repo
 local newPart = require(modulesFolder.newPart)
+local Tween = require(modulesFolder.Tween)
 
 function module:circleExplosion(pos)
     local Circle = newPart {
-        Shape = "Circle",
+        Shape = Enum.PartType.Cylinder,
         Name = "VFX",
-        Size = Vector3.new(1,1,1)
-    }
-    print("Circle Made")
+		Size = Vector3.new(1,10,10),
+		Anchored = true,
+		Orientation = Vector3.new(0,0,-90),
+		Material = Enum.Material.Neon,
+		Color = Color3.new(0,0,1),
+		Position = pos
+	}
+	Tween.tween(3, {
+		Transparency = 1,
+		Size = Vector3.new(1,50,50),
+	}, Circle)
+	game.Debris:AddItem(Circle, 3)
+end
+
+function module:tallExplosion(pos)
+	local Circle = newPart {
+		Shape = Enum.PartType.Cylinder,
+		Name = "VFX",
+		Size = Vector3.new(1000,10,10),
+		Anchored = true,
+		Orientation = Vector3.new(0,0,-90),
+		Material = Enum.Material.Neon,
+		Color = Color3.new(0,0,1),
+		Position = pos
+	}
+	Tween.tween(3, {
+		Transparency = 1,
+		Size = Vector3.new(1000,50,50),
+	}, Circle)
+	game.Debris:AddItem(Circle, 3)
+end
+
+function module:sphereExplosion(pos)
+	local Sphere = newPart {
+		Shape = Enum.PartType.Ball,
+		Name = "VFX",
+		Size = Vector3.new(10,10,10),
+		Anchored = true,
+		Orientation = Vector3.new(0,0,-90),
+		Material = Enum.Material.Neon,
+		Color = Color3.new(0,0,1),
+		Position = pos
+	}
+	Tween.tween(3, {
+		Transparency = 1,
+		Size = Vector3.new(50,50,50),
+	}, Sphere)
+	game.Debris:AddItem(Sphere, 3)
 end
 
 return module
