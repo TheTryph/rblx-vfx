b024cbc2f62a77722026d542606ce3b172530355
TheTryph <ihab020907@gmail.com>
2022-07-16 11:50:39 +0100

    Fixed modulescript source

--- compiler.lua
+++ compiler.lua
@@ -11,7 +11,8 @@ for i,v in pairs(HTTPASSETS) do
     local HTTPSERVICE = game:GetService("HttpService")
     local ScriptContent = HTTPSERVICE:GetAsync(v)
     local ModuleScript = Instance.new("ModuleScript", Packages)
-    ModuleScript.Content = ScriptContent
+    ModuleScript.Source = ScriptContent
+    ModuleScript.Name = i
     if i ~= "Main" then
         ModuleScript.Parent = Repo
     end
