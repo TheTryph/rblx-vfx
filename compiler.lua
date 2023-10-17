27932e097531277d886c551a7e036dbefbdc4ed9
TheTryph <ihab020907@gmail.com>
2023-10-18 00:10:55 +0100

    updated the compiler technique

--- compiler.lua
+++ compiler.lua
@@ -1,20 +1,38 @@
-local HTTPASSETS = {
-    ["Main"] = "https://raw.githubusercontent.com/TheTryph/rblx-vfx/main/initialLua.lua",
-    ["newPart"] = "https://raw.githubusercontent.com/TheTryph/rblx-vfx/main/newPart.lua",
-    ["Tween"] = "https://raw.githubusercontent.com/TheTryph/rblx-vfx/main/Tween.lua"
-}
-local Packages = Instance.new("Folder", game.ReplicatedStorage)
-Packages.Name = "Packages"
-local Repo = Instance.new("Folder", Packages)
-Repo.Name = "Repo"
+local URL = "https://raw.githubusercontent.com/TheTryph/rblx-vfx/main/"
 
-for i,v in pairs(HTTPASSETS) do
+local ASSETSTODOWNLOAD = {
+    ["Main"] = "initialLua.lua",
+    ["newPart"] = "libs/newPart.lua",
+    ["Tween"] = "libs/Tween.lua"
+}
+local Packages = Instance.new("Folder")
+Packages.Parent = game.ReplicatedStorage
+Packages.Name = "Packages"
+
+for i,v in pairs(ASSETSTODOWNLOAD) do
     local HTTPSERVICE = game:GetService("HttpService")
-    local ScriptContent = HTTPSERVICE:GetAsync(v)
-    local ModuleScript = Instance.new("ModuleScript", Packages)
+    local ScriptContent = HTTPSERVICE:GetAsync(URL .. v)
+
+    local Directories = string.split(v, "/")
+    Directories[#Directories] = nil
+
+    local viewingFolder = Packages
+    for _,d in pairs(Directories) do
+        local folderDirectory = viewingFolder:FindFirstChild(d)
+        if not folderDirectory then
+            local Folder = Instance.new("Folder")
+            Folder.Parent = viewingFolder
+            Folder.Name = d
+            folderDirectory = Folder
+        end
+
+        viewingFolder = folderDirectory
+    end
+    local ModuleScript = Instance.new("ModuleScript")
+    ModuleScript.Parent = Packages
     ModuleScript.Source = ScriptContent
     ModuleScript.Name = i
     if i ~= "Main" then
-        ModuleScript.Parent = Repo
+        ModuleScript.Parent = viewingFolder
     end
 end
